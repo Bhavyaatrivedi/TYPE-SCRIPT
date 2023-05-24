75016eb1ea4831adf7e10aafcc7e41c9d2833625
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-05-24 10:05:08 +0530

    tuple

---
+++ LESSON 3/src/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'John';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('Jim');
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+//Tuple
+let myTuple = ['Dev', 543267, false];
+let mixed = ['John', 8623, true];
+mixed = myTuple;
+myTuple[1] = 42;
---
+++ LESSON 3/src/main.ts
@@ -0,0 +1,23 @@
+let stringArr = ['one', 'hey', 'Dave']
+
+let guitars = ['Strat', 'Les Paul', 5150]
+
+let mixedData = ['EVH', 1984, true]
+
+stringArr[0] = 'John'
+stringArr.push('hey')
+
+guitars[0] = 1984
+guitars.unshift('Jim')
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+//Tuple
+let myTuple : [string,number, boolean] = ['Dev',543267, false]
+let mixed= ['John', 8623, true]
+
+mixed= myTuple
+myTuple[1] = 42
+
